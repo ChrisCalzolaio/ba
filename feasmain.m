ddd10f82aadb399f1df2f24854a216acf03cb3ce
ChrisCalzolaio <calzolaiochris@gmail.com>
2020-04-20 00:42:41 +0200

    moved plotting implementation of a basic 3D approach rotation of the blade around its z(?)-axis implementation of a logging metadata output

--- feasmain.m
+++ feasmain.m
@@ -1,39 +1,50 @@
+%% setup
+% simulation
+sim = struct();                     % init a struct, sim is technically an inbuilt function...
+sim.rotation = 90;
+sim.steps = 1e3;
+sim.logt = table('Size',[0,4],...
+                'VariableTypes',{'uint64','double','double','double'},...
+                'VariableNames',{'step','xpos','rota','step_exec_time'});        % maybe add start of execution time?
+% graphical output
+
+
 %% creation of objects
 % the material
-mat.od = [10,10];          % material outer dimensions [x,y]
-mat.pos = [0,0];            % material coordinate system position
+mat.od = [10,10,10];                                            % [x,y,z]: material outer dimensions
+mat.pos = [0,0,0];                                              % [x,y,z]: material coordinate system position within the the global coordsys
 mat.local.vertices = rectangleVert(mat.od,'lowerleft');
-mat.global.vertices = mat.local.vertices + mat.pos;
+mat.global.vertices = mat.local.vertices + mat.pos(1:2);
 mat.pgon = polyshape(mat.global.vertices);
 % blade, the cutting feature
-blade.od = [1,1];       % blade outer dimensions [x,y]
-blade.pos = [mat.od(1)/2,mat.od(2)];
+blade.od =  [1,1,0];                                             % [x,y,z]: blade outer dimensions
+blade.pos = [0,mat.od(1)/2,mat.od(2)];                          % [x,y,z]: blade coordinate system position in global coordinate system
 blade.local.vertices = rectangleVert(blade.od,'center');
-blade.global.vertices = blade.local.vertices + blade.pos;
+blade.global.vertices = blade.local.vertices + blade.pos(2:3);
 blade.pgon = polyshape(blade.global.vertices);
-% the part, the result of the cutting operation
-part = mat;
-part.pgon = mat.pgon.subtract(blade.pgon);
 
-%% plotting
-face.Color = [0 0.447 0.741];
-face.alpha = 0.35;
-
-figH = findobj('type','figure');
-if logical(numel(figH))
-    figH = figH(1);
-    set(0,'CurrentFigure',figH);clf;
-else
-    figH = figure();
+%% simulation
+fprintf(1,'[ %s ] Simulation setup.\n',datestr(now,'HH:mm:SS'));
+sim.stepsize.x = mat.od(3)/sim.steps;
+sim.stepsize.rot = sim.rotation/sim.steps;
+% preallocation of RAM
+part = repmat(mat,sim.steps,1);                                     % the part, the result of the cutting operation
+fprintf(1,'[ %s ] starting simulation...\n',datestr(now,'HH:mm:SS'));
+simtime = tic;
+for step=1:(sim.steps + 1)
+    xpos = (step-1) * sim.stepsize.x;
+    rota = (step-1) * sim.stepsize.rot;
+    curblade = blade.pgon.rotate(rota,blade.pos(2:3));
+       
+    stepdur = tic;
+    
+    part(step).pgon = mat.pgon.subtract(curblade);
+    part(step).pos(3) = xpos;
+    
+    fprintf(1,'[ %s ] step %i @ xpos=%.3f mm and rota=%.3f° took %.4e sec.\n',datestr(now,'HH:mm:SS'),step,xpos,rota,toc(stepdur));
+    sim.logt{step,:} = [step,xpos,rota,toc(stepdur)];
 end
-
-axH(1) = subplot(1,2,1);
-patch('Faces',1:length(mat.pgon.Vertices),'Vertices',mat.pgon.Vertices,'FaceColor',face.Color,'FaceAlpha',face.alpha)
-patch('Faces',[1 2 3 4],'Vertices',blade.pgon.Vertices,'FaceColor',mod(face.Color+.5,1),'FaceAlpha',face.alpha)
-axH(1).DataAspectRatio = [1 1 1];
-grid on; grid minor;
-
-axH(2) = subplot(1,2,2);
-patch('Faces',1:length(part.pgon.Vertices),'Vertices',part.pgon.Vertices,'FaceColor',face.Color,'FaceAlpha',face.alpha)
-axH(2).DataAspectRatio = [1 1 1];
-grid on; grid minor;
+fprintf(1,['[ %s ] finished Simulation.\n', ...
+           '\t\t\t%i steps took %.3f sec.\n', ...
+           '\t\t\tsingle step cummulative time was %.3f sec.\n'],....
+           datestr(now,'HH:mm:SS'),step,toc(simtime),sum(sim.logt.step_exec_time));
