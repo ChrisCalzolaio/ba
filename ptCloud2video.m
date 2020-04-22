268e4d5389607626c070e86227205784deea830c
ChrisCalzolaio <calzolaiochris@gmail.com>
2020-04-22 21:47:30 +0200

    tilt movement of the camera during rotation + two different functions to calculate a tilt angle
    implemented video output
    
    changed framerate

--- ptCloud2video.m
+++ ptCloud2video.m
@@ -1,6 +1,7 @@
 recfull = true;
+outputv = true;
 rotation = 'full';
-framerate = 30;
+framerate = 90;
 duration = 10;
 frames = framerate * duration;
 figH = getFigH(1);
@@ -21,22 +22,60 @@ else
     figH.WindowStyle = 'docked';
 end
 
+if outputv
+    drawnow();
+    M = repmat(getframe(figH),numel(part),1);  % grab a dummy frame and preallocate RAM with it
+end
+
 % define equation for camera azimuth movement
 % we use a upside down parabula normalized within a range of 1
 % this allows scaling of output values by a simple prefactor
-pos = .5;
-x = linspace(0,1,frames);
-y = -((1/pos)^2)*(x-pos).^2 + 1;
+horizMode = 'spline';
+switch horizMode
+    case 'sin'
+        pos = .5;
+        x = linspace(0,1,frames);
+        elevang = -((1/pos)^2)*(x-pos).^2 + 1;
+        clearvars x
+    case 'spline'               % ToDo: find proper way to create the Bezier curve
+        fixpt = [-10,0;...
+            25,0;...
+            ((frames/2)-25), 1;...
+            ((frames/2)+25), 1;...
+            (frames-25),0;...
+            frames+20 , 0];
+        elevang = pchip(fixpt(:,1),fixpt(:,2),1:frames);
+        clearvars fixpt
+end
 
 [az,el] = view();
 for frame=0:frames
-% view(az+frame*(angle/frames),el);
-camorbit(angle/frames,0,'data')
-fprintf('campos x=%.1f y=%.1f z=%.1f with PlotBoxAspRat %.3f %.3f %.3f and DataAspRat %.3f %.3f %.3f\n',axH.CameraPosition,axH.PlotBoxAspectRatio,axH.DataAspectRatio)
-drawnow();
+    view(az+frame*(angle/frames),el - 20*( -1/pos^2 * (frame/frames - pos).^2 + 1));
+    % camorbit(angle/frames,0,'data')
+    fprintf('Camera Azimuth %.3f\tElevation %.3f\n',axH.View)
+    drawnow();
+    
+    if outputv
+        M(frame+1) = getframe(figH);                                          % grab the frame
+    end
 end
 
 if recfull
     figH.WindowState = 'normal';
     figH.WindowStyle = 'docked';
+end
+if outputv
+    io.name = strcat(datestr(now,'yyyy-mm-dd_HH-MM-SS'),'_ptCloud_export');
+    io.path = strcat('D:\temp\exports\',datestr(now,'yyyy-mm-dd'));
+    io.ff = fullfile(io.path,io.name);
+    if ~exist(io.path,'dir')                    % make directory if it doesn't exist yet
+        mkdir(io.path)
+    end
+    
+    v = VideoWriter(io.ff);
+    v.FrameRate = framerate;
+    
+    open(v)
+    writeVideo(v,M)         % Write the matrix of data M to the video file.
+    close(v)                % Close the file.
 end
