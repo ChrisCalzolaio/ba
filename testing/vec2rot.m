2747a40bec17ac81140745fc0c236603adcb917a
ChrisCalzolaio <calzolaiochris@gmail.com>
2020-07-21 17:05:13 +0200

    initial commit

---
+++ testing/vec2rot.m
@@ -0,0 +1,20 @@
+function [R] = vec2rot(a,b)
+%VEC2ROT Rotational transform matrix from basis and projected vector
+%   probably doesn't work
+%   ToDo: find source for this algorithm
+
+dim = length(a);
+
+v = cross(a,b);
+s = norm(v);
+c = dot(a,b);
+
+R = eye(dim) + skew(v) + skew(v).^2.*((1-c)/s^2);
+
+    function skw = skew(v)
+        skw = [0 -v(3) v(2);...
+               v(3) 0 -v(1);...
+               -v(2) v(1) 0];
+    end
+end
+
