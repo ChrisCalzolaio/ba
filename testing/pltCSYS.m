fc260b7da26134a007eebd8d4caaea61ee4e5260
ChrisCalzolaio <calzolaiochris@gmail.com>
2020-07-27 15:00:11 +0200

    make parent axis 3d

--- testing/pltCSYS.m
+++ testing/pltCSYS.m
@@ -1,12 +1,38 @@
 function [csysH] = pltCSYS(origin,drct,varargin)
 %csysH = PLTCSYS(origin,direction) plots coordinate system using line
 
+
 pltarray = reshape([origin(:),drct(:)]',6,3);
 csysH = line(pltarray(:,1),pltarray(:,2),pltarray(:,3),varargin{:});
 
 txts = {'x','y','z'};
 for dim=1:3
 	text(drct(dim,1),drct(dim,2),drct(dim,3),txts{dim},varargin{:});
+end
+
+% detect if provided axis object is 3d
+% ref: https://stackoverflow.com/questions/8064969
+% modification: we look at the azimuth value, since it is only 0, if axis
+% is 2d
+% if axis was 3d, but the view angles were different from default, the
+% shown method won't work. this is a hacky workaround...
+axH = csysH.Parent;                             % we cannot use the Parent argument from varargin, since it's optional
+tries = 1;
+while true                                      % the parent handle of the line handle is not an axis, we try to find on up the tree
+    if strcmp(get(axH,'Type'),'axes')           % if our current handle is type axis, stop searching
+        break
+    else
+        axH = axH.Parent;                       % if not, look at parent within tree
+    end
+    if tries==3
+        error('Error using pltCSYS.\nUnable to find a parent axis handle for the provided Figure.')
+    end
+end
+ornt = axH.View;                                % get orientation
+if ~logical(ornt(1))                            % first value is the azimuth
+    set(axH.Parent,'CurrentAxes',axH);          % make target axis current
+    view(3);
+    axis vis3d
 end
 
 end
