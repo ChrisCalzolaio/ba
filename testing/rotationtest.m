107ef916d86ac2c335a9e91e897f26b78b578a5e
ChrisCalzolaio <calzolaiochris@gmail.com>
2020-07-28 13:10:58 +0200

    testing for the more efficient version: works

--- testing/rotationtest.m
+++ testing/rotationtest.m
@@ -14,7 +14,7 @@ ssc = @(v) [0 -v(3) v(2);...
             v(3) 0 -v(1);...
             -v(2) v(1) 0];
 
-RU = @(A,B) eye(3) + ssc(cross(A,B)) + ssc(cross(A,B))^2*(1-dot(A,B))/(norm(cross(A,B))^2)
+RU = @(A,B) eye(3) + ssc(cross(A,B)) + ssc(cross(A,B))^2*(1-dot(A,B))/(norm(cross(A,B))^2);
 
 vu = @(v) v/norm(v);            % function to normalize the vectors
 ru = @() vu(rand(3,1));         % function to create random vectors
@@ -25,13 +25,18 @@ b=[1 1 1]';
 a = vu(a);                      % normalize vectors
 b = vu(b);
 
-U = UU(FFi(a,b), GG(a,b));
-fprintf('Is it length-preserving:\nnorm(U): %d\n',norm(U))
+U_old = UU(FFi(a,b), GG(a,b));
+U_new = RU(a,b);
+fprintf('[old] Is it length-preserving:\nnorm(U): %d\n',norm(U_old))
+fprintf('[new] Is it length-preserving:\nnorm(U): %d\n',norm(U_new))
 
-fprintf('Does it rotate a onto b?\nnorm(b-U*a): %d\n',norm(b-U*a))
+fprintf('[old] Does it rotate a onto b?\nnorm(b-U*a): %d\n',norm(b-U_old*a))
+fprintf('[new] Does it rotate a onto b?\nnorm(b-U*a): %d\n',norm(b-U_new*a))
 
-fprintf('U * a\n')
-c = U * a
+fprintf('[old] U * a\n')
+c = U_old * a
+fprintf('[new] U * a\n')
+c = U_new * a
 
 origin = zeros(3);
 csys = eye(3);
@@ -43,6 +48,6 @@ quH = quiver3simple(zeros(3,1),[a,b,c]);
 % hold on;
 % plot coordinate systems
 pltCSYS(origin,csys,'Color','r');
-csysrot = csys * U';
+csysrot = csys * U_old';
 pltCSYS(origin,csysrot,'Color','g');
 % hold off;
