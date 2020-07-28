5ba4a8be94f5a0e9a7312bf6f6672e94a826a48a
ChrisCalzolaio <calzolaiochris@gmail.com>
2020-07-28 15:09:18 +0200

    added transformation directions

--- dim4.m
+++ dim4.m
@@ -1,20 +1,33 @@
-function [A] = dim4(A,type)
+function [A] = dim4(A,type,direction)
 %DIM4 Returns 4-dimensional version of 3 dimensional vectors or transform
 %matrices
+% Direction:
+%       - forward: transform from 3d to 4d
+%       - backward: tranform from 4d to 3d
 % better handling of edge cases required
 
 switch type
     case 1                      % one dimensioal, vectors
         ncols = size(A,2);      % number of column vectors in the matrix of vectors
-        A = [A;ones(1,ncols)];
+        switch direction
+            case 'forward'
+                A = [A;ones(1,ncols)];
+            case 'backward'
+                A = A(1:3,:);
+        end
     case 2                      % two dimensional, (transform) matrics
         [nrows,ncols] = size(A);
         if ~(nrows==ncols)      % check size conformity
             error('Provided transform matrix is not sqare.')
         end
-        tmp = eye(4);
-        tmp(1:3,1:3) = A;
-        A = tmp;
+        switch direction
+            case 'forward'
+                tmp = eye(4);
+                tmp(1:3,1:3) = A;
+                A = tmp;
+            case 'backward'
+                A = A(1:3,1:3);
+        end
 end
 end
 
