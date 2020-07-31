4fa0f346fad072890671861fecd7dcb4a01238f1
ChrisCalzolaio <calzolaiochris@gmail.com>
2020-07-31 11:47:39 +0200

    applytm: - fix: return transformed 2xn vector, when 2xn vector is provided dim4: - fix: add general z-values to 2xn matrices of vectors, to be able to treat the vectors as 3d rectangleVert: - fix: return matrix of colum vectors:    - convert provided dimensions to colum vector    - changed creation of vertices to return a matrix of vectors rectangleVerttesting: - commented out currently unnesseray tests

--- fun/applytm/applytm.m
+++ fun/applytm/applytm.m
@@ -2,6 +2,7 @@ function [vec] = applytm(vec,TM)
 %APPLYTM Apply transformation matrix
 %   Detailed explanation goes here
 
+nrowsvec = size(vec,1);
 sTM = size(TM);
 if ~(sTM(1) == sTM(2))
     error('Transformation dimensions incorrect')
@@ -14,5 +15,11 @@ switch sTM     % determine type of transformation matrix provided
 end
 % apply transformation
 vec = dim4(TM' * dim4(vec,1,'forward'),1,'backward');
+% if the vector matrix was size 2, so x,y information, we converted it to
+% a generix x,y,z vector matrix, with the z-values being 0, but we only
+% want to return an x,y vector matrix again
+if nrowsvec==2
+    vec = vec(1:2,:);
+end
 end
 
