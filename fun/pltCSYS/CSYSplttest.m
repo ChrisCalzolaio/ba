1af2840ab36817bbaa33231eab8a092bd3a71a0a
ChrisCalzolaio <calzolaiochris@gmail.com>
2020-09-08 14:11:36 +0200

    fix:  - changed call to the getFigH function, the createaxis argument is not set, since two output arguments are specified

--- fun/pltCSYS/CSYSplttest.m
+++ fun/pltCSYS/CSYSplttest.m
@@ -2,7 +2,7 @@ origin = zeros(3);
 csys = eye(3);
 
 
-[figH,axH] = getFigH(2,'createaxis');
+[figH,axH] = getFigH(2);
 % set(0,'CurrentFigure',figH(1))
 line(axH(1),linspace(0,2*pi),sin(linspace(0,2*pi)))
 pltCSYS(origin,csys,'Color','r','Parent',axH(1));
