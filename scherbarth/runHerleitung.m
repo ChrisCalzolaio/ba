bef968b5d9d1630000267b518587efddca3c4613
Chris Schuster <calzolaiochris@gmail.com>
2020-10-27 12:47:47 +0100

    hobbing.m - removed definition of unused variables - better rounding function (fix); rounds towards zero (important for negativ numbers) - changed name of machine transformation matrices - moved explanation of solution methods to seperate scripts hobbing2: - moved solution method using fzero to seperate script losungBkartes: - added useful title - added call to the hobbing script, to get the Variables and Gesamt - definition of z_soll, because it is actually used here losungBpolar: - added usefull title - added call to the hobbing script, to get the Variables and Gesamt - definition of z_soll, because it is actually used here runHerleitung: - updated name of script to run Lösungsverfahren: - added script for each   - this allows deeper analysis and work on the methods behaviourSymsDatatype: - script to understand behaviour of the sym data type

--- scherbarth/runHerleitung.m
+++ scherbarth/runHerleitung.m
@@ -2,7 +2,7 @@ clearvars;
 herlT = tic;
 
 out = evalc('hobbing');
-out = evalc('losungBfun');
+out = evalc('losungBkartes');
 out = evalc('hobbing2');
 
 fprintf('[ %s ] Running the Herleitung took %.3f sec.\n',datestr(now,'HH:mm:ss'),toc(herlT));
