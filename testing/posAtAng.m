d521363d31ffab9fe62210f087e619ddab91d46e
Chris Schuster <calzolaiochris@gmail.com>
2020-11-12 22:09:03 +0100

    - vf1   - added creation of tool coordinates using the WZ-function   - definition of additional variables was required   - removed the standard poi definition   - added calculation of analytic trajectory with the same values to plot against and compare   - changed z-axis scope of simulation   - changed the bfun: h_WZ coordinate component was added instead of substracted, also moved into the sin(A) bracket   - redesigned to write the simulation results in 3d matrices, the third dimension is for timesteps   - renamed the variable error - posAtAng   - added logic to select which analytic data to plot against   - changed some plot calls to cope with the 3d simulation results

--- testing/posAtAng.m
+++ testing/posAtAng.m
@@ -1,33 +1,47 @@
+data = 'traj';
+switch data
+    case 'sim'
+        load('simRes.mat');
+        traj = anaR.anaCord;
+        Bvec = anaR.angB;
+        clearvars anaR simR
+        nP = 1;
+    case 'traj'
+        nP = size(traj,2);
+end
+
+nP = 1;     % point selector
+
 %% graphics
 [figH,axH] = getFigH(1,'WindowStyle','keep');
 set(0,'CurrentFigure',figH);
 tH = tiledlayout(figH,5,1);
 
 axH(1) = nexttile(tH,1,[3 1]);
-lH(1) = line(axH,anaR.angB,anaR.anaCord(:,3),'Color','#EDB120');
-lH(2) = line(Bsol,z_soll,'color','#A2142F','Marker','.');
+lH(1) = line(axH,shiftdim( Bvec),shiftdim(traj(3,nP,:)),'Color','#EDB120');
+lH(2) = line(shiftdim(Bsol(1,nP,:)),z_soll,'color','#A2142F','Marker','.');
 legend('Z sim','Z iter');
 
 axH(2) = nexttile(tH,4);
-lH(3) = line(Bsol,iters);
+lH(3) = line(shiftdim(Bsol(1,nP,:)),iters);
 
 axH(3) = nexttile(tH,5);
-lh(4) = line(Bsol,error);
+lh(4) = line(shiftdim(Bsol(1,nP,:)),err);
 
 
 xlims = [0 2];
 linkaxes(axH,'x')
-axH(1).XLim = xlims;
+% axH(1).XLim = xlims;
 % axH.YLim = ylims;
 axSetup();
 
 %% numeric
 % start value
-[~,indsv] = min(abs(anaR.angB - B0));
+[~,indsv] = min(abs(shiftdim(Bvec) - B0(nP)));
 datatip(lH(3),'DataIndex',indsv);
 % calculated value
 if isa(B, 'sym')
     B = double(B(1));
 end
-[~,indcv] = min(abs(anaR.angB - B));
+[~,indcv] = min(abs(Bvec - B));
 datatip(lH(3),'DataIndex',indcv);
