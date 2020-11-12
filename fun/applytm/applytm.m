9032e5ca521a34c85f1270f6e52afce6d5cd7834
Chris Schuster <calzolaiochris@gmail.com>
2020-11-12 16:54:15 +0100

    fix indentation

--- fun/applytm/applytm.m
+++ fun/applytm/applytm.m
@@ -31,15 +31,15 @@ end
 if any([vecIsSym,matIsSym])
     vecout = TM * dim4(vecin,1,'forward');
 else
-% apply transformation
-vecout = dim4(pagemtimes( TM , dim4(vecin,1,'forward')),1,'backward');
-% return point cloud with one iteration (time) step
-vecout = reshape(vecout,3,sVec(2)*sTM(3));
+    % apply transformation
+    vecout = dim4(pagemtimes( TM , dim4(vecin,1,'forward')),1,'backward');
+    % return point cloud with one iteration (time) step
+    vecout = reshape(vecout,3,sVec(2)*sTM(3));
 end
 
 if sVec(1)==2
-% if the vector matrix was size 2, so x,y information, we converted it to
-% homogeneous coordinates (z-value = 1), return only the provided information
+    % if the vector matrix was size 2, so x,y information, we converted it to
+    % homogeneous coordinates (z-value = 1), return only the provided information
     vecout = vecout(1:2,:);
 end
 end
